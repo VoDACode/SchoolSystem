a9e2db80fb4946b7dff8f2f413b6e064c6933b30
VoDA <dimavovk201112@gmail.com>
2022-11-14 23:00:07 +0200

    Implemented page "create-first-user"

--- SchoolSystem/ClientApp/src/app/create-first-user/create-first-user.component.ts
+++ SchoolSystem/ClientApp/src/app/create-first-user/create-first-user.component.ts
@@ -28,22 +28,21 @@ export class CreateFirstUserComponent implements AfterViewInit {
   constructor() { }
 
   ngAfterViewInit(): void {
-    var dtToday = new Date();
+    var today = new Date();
 
-    var month: any = dtToday.getMonth() + 1;
-    var day: any = dtToday.getDate();
-    var year = dtToday.getFullYear();
+    var month: any = today.getMonth() + 1;
+    var day: any = today.getDate();
+    var year = today.getFullYear();
 
-    if (month < 10)
-      month = '0' + month.toString();
-    if (day < 10)
-      day = '0' + day.toString();
-
-    var minDate = (year - 12) + '-' + month + '-' + day;
+    var minDate = (year - 200) + '-' + month + '-' + day;
     this.inputBirthday?.nativeElement.setAttribute('min', minDate);
+    today.setMonth(today.getMonth() + 1);
+    this.inputBirthday?.nativeElement.setAttribute('max', `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`);
   }
 
   submit(): void {
+    let valid = true;
+
     this.firstNameError!.nativeElement.innerHTML = '';
     this.lastNameError!.nativeElement.innerHTML = '';
     this.middleNameError!.nativeElement.innerHTML = '';
@@ -54,23 +53,45 @@ export class CreateFirstUserComponent implements AfterViewInit {
 
     if (this.model.firstName == undefined || this.model.firstName.length < 2) {
       this.firstNameError!.nativeElement.innerHTML = "Ім'я не може бути коротшим за 2 символи";
+      valid = false;
     }
     if (this.model.lastName == undefined || this.model.lastName.length < 2) {
       this.lastNameError!.nativeElement.innerHTML = "Прізвище не може бути коротшим за 2 символ";
+      valid = false;
     }
     if (this.model.middleName !== undefined && (this.model.middleName?.length != 0 && (this.model.middleName?.length || 0) < 2)) {
       this.middleNameError!.nativeElement.innerHTML = "По батькові не може бути коротшим за 2 символи";
+      valid = false;
     }
     if (this.model.phone == undefined || this.phoneRegExp.test(this.model.phone) == false) {
       this.phoneError!.nativeElement.innerHTML = "Не вірний формат номеру телефону (Приклад: +380123456789)";
+      valid = false;
     }
     if (this.model.email == undefined || this.emailRegExp.test(this.model.email) == false) {
       this.emailError!.nativeElement.innerHTML = "Не вірний формат електронної пошти (Приклад: email@example.com)";
+      valid = false;
     }
     if (this.model.login == undefined) {
       this.loginError!.nativeElement.innerHTML = "Логін не може бути пустим";
+      valid = false;
     } else if (this.model.login.length < 3) {
       this.loginError!.nativeElement.innerHTML = "Логін не може бути коротшим за 4 символи";
+      valid = false;
+    }
+
+    if (this.model.password == undefined || this.model.password.length == 0) {
+      this.passwordError!.nativeElement.innerHTML = "Пароль не може бути пустим";
+      valid = false;
+    } else if (this.model.password.length < 8) {
+      this.passwordError!.nativeElement.innerHTML = "Пароль не може бути коротшим за 8 символів";
+      valid = false;
+    } else if (this.model.password != this.model.passwordConfirm) {
+      this.passwordError!.nativeElement.innerHTML = "Паролі не співпадають";
+      valid = false;
+    }
+
+    if (valid) {
+      console.log(this.model);
     }
   }
 
@@ -86,5 +107,9 @@ export class CreateFirstUserComponent implements AfterViewInit {
     if (this.model.phone == undefined || this.phoneRegExp.test(this.model.phone) == false) {
       this.phoneError!.nativeElement.innerHTML = "Не вірний формат номеру телефону (Приклад: +380123456789)";
     }
+  }
+
+  clearError(element: HTMLSpanElement): void {
+    element.innerHTML = '';
   }
 }
